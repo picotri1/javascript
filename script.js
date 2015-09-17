80d7a7aed153d16ae79088e04f29851b246d26ae
picotri1 <herrero.carlos17@csj-rpi.org>
2015-09-17 09:33:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+function changeImage() {
+  var image = document.getElementById('myImage');
+  
+  ir (image.src.match('bulbon')){
+    image.src = "image/pic_bulboff.gif";
+  } else {
+    image.src = "image/pic_bulbon.gif"
+  }
+  }
+}
